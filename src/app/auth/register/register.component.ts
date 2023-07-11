92c27b7baa554439348201230e0bd0c85b721d2f
yoanarobeva <yoana_robeva@yahoo.com>
2023-07-11 14:13:15 +0300

    register log user for now

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
-
+  constructor(private authService: AuthService, private router: Router) {}
+  
+  register(username: string, email: string, phone: string, password: string, rePassword: string) {
+    this.authService.login();
+    this.router.navigate(['/']);
+  }
 }
