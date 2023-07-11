a78fd78ad2f418c2d9167657bbb7c32210278f52
yoanarobeva <yoana_robeva@yahoo.com>
2023-07-11 11:21:00 +0300

    themes fixes

--- src/app/themes/themes.module.ts
+++ src/app/themes/themes.module.ts
@@ -20,7 +20,6 @@ import { SharedModule } from '../shared/shared.module';
     CommonModule,
     ThemesRoutingModule,
     SharedModule,
-    ThemesRoutingModule,
   ]
 })
 export class ThemesModule { }
