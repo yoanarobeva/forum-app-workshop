d3d863db33f8ef0dc40294b369b6f72099ac13c4
yoanarobeva <yoana_robeva@yahoo.com>
2023-07-23 22:52:36 +0300

    custom pipes added

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,10 +4,12 @@ import { LoaderComponent } from './loader/loader.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { RouterModule } from '@angular/router';
 import { EmailDirective } from './validators/email.directive';
+import { SlicePipe } from './pipes/slice.pipe';
+import { ElapsedTimePipe } from './pipes/elapsed-time.pipe';
 
 @NgModule({
-  declarations: [LoaderComponent, WelcomeComponent, EmailDirective],
+  declarations: [LoaderComponent, WelcomeComponent, EmailDirective, SlicePipe, ElapsedTimePipe],
   imports: [CommonModule, RouterModule],
-  exports: [LoaderComponent, WelcomeComponent],
+  exports: [LoaderComponent, WelcomeComponent, SlicePipe, ElapsedTimePipe],
 })
 export class SharedModule {}
